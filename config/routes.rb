17b935542a4d48ce51f35105066d080d62e65865
Yulia_Lime <lime666lime@gmail.com>
2022-04-08 17:38:04 +0300

    need to fix routes for crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
+
+  get 'admin', to: 'admin/users#index'
+  get '/admin/users/show', to: 'admin/users#show', as: 'user'
+  get '/admin/users/new', to: 'admin/users#new', as: 'new_user_path'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
