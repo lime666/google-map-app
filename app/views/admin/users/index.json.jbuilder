d7219f130f0bdbf1b015d3a92dd0a64e2b7498d8
Yulia_Lime <lime666lime@gmail.com>
2022-04-11 12:36:24 +0300

    can't find user

--- app/views/admin/users/index.json.jbuilder
+++ app/views/admin/users/index.json.jbuilder
@@ -1,1 +1,4 @@
-json.array! @users, partial: "users/user", as: :user
+#json.array! @users, partial: "admin/users/user", locals: {user: user}, as: :user
+json.array! @users do |user|
+  json.html render partial: 'user', locals: { user: user }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   end
 
   get 'admin', to: 'admin/users#index'
-  get '/admin/users/show', to: 'admin/users#show', as: 'user'
-  get '/admin/users/new', to: 'admin/users#new', as: 'new_user_path'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #get '/admin/users/show', to: 'admin/users#show'
+  #get '/admin/users/new', to: 'admin/users#new'
+  #delete 'delete_user', to: 'admin/users#destroy'
 
-  # Defines the root path route ("/")
   # root "articles#index"
 end
